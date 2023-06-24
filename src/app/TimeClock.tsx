32e3c04d5841023abc90464932cafccab810cb89
edgarmejiav <edgarm_10@hotmail.com>
2023-06-24 14:23:57 -0500

    fix : add support scream phone

--- src/app/TimeClock.tsx
+++ src/app/TimeClock.tsx
@@ -11,7 +11,12 @@ export const TimeClock = () => {
             const hours = time.getHours();
             const minutes = time.getMinutes();
             const seconds = time.getSeconds();
-            setTimeCurrent(`${hours}:${minutes}:${seconds}`);
+
+            const formattedHours = hours < 10 ? `0${hours}` : hours;
+            const formattedMinutes = minutes < 10 ? `0${minutes}` : minutes;
+            const formattedSeconds = seconds < 10 ? `0${seconds}` : seconds;
+
+            setTimeCurrent(`${formattedHours}:${formattedMinutes}:${formattedSeconds}`);
         }, 1000);
 
         return () => {
